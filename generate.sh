415c11a760fe8d27851f3d52af20a044a03990c4
Mike Schaeffer <mike.schaeffer@digitalasset.com>
2021-07-20 07:12:22 -0400

    Modify the generate script to actually generate the releases.

--- generate.sh
+++ generate.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
-for ii in $(seq 0 10);
+for ii in $(seq 0 99);
 do
-    cat dabl-meta.template.yaml | sed s/XYZZY/${ii}/
+    cat dabl-meta.template.yaml | sed s/XYZZY/${ii}/ > dabl-meta.yaml
+
+    ddit clean
+    ddit build
+    ddit release
 done
